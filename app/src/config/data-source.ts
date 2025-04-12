96332561cfc34689d78d28904cb8456b0a6183bd
Daris02 <hei.raymond.2@gmail.com>
2025-04-12 09:37:25 +0300

    feat: setup var env

--- app/src/config/data-source.ts
+++ app/src/config/data-source.ts
@@ -2,8 +2,6 @@ import { DataSource } from 'typeorm';
 import { postgres } from './config';
 import { User } from '../models/entities/User';
 
-console.log('Postgres URL:', postgres.pgUrl);
-
 const AppDataSource = new DataSource({
   type: 'postgres',
   url: postgres.pgUrl,
